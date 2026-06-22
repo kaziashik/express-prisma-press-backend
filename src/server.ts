0e7a1fb0193c8f89592607027141bad14e1c495b
kaziashik <ashik@apppay.tech>
2026-06-22 08:26:30 +0800

    initial commit - express prisma setup

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
+import "dotenv/config";
+
+const PORT = config.port;
+
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("connected to the database successfuly");
+    app.listen(PORT, () => {
+      console.log(`Server is runnng on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error starting the  server: ", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
